8c3f3520975968d407817454bd4dafff6daffaf7
txidol <tanxiang163@163.com>
2014-07-03 16:21:14 +0800

    init

---
+++ src/com/goldheaven/IndexActivity.java
@@ -0,0 +1,14 @@
+package com.goldheaven;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IndexActivity extends Activity{
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+	
+}
