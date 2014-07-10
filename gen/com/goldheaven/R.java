20992919e52afb6ed0c35c32cb9f41fe878315f2
txidol <tanxiang163@163.com>
2014-07-10 10:21:30 +0800

    mod

--- gen/com/goldheaven/R.java
+++ gen/com/goldheaven/R.java
@@ -13,12 +13,20 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int buttonUpdate=0x7f060001;
+        public static final int editText=0x7f060000;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int status=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int buttonUpdate=0x7f040004;
         public static final int hello=0x7f040001;
+        public static final int hintText=0x7f040003;
+        public static final int titleStatus=0x7f040002;
     }
     public static final class style {
         /** 
@@ -43,6 +51,9 @@ public final class R {
  API 14 theme customizations can go here. 
          */
         public static final int AppBaseTheme=0x7f050000;
+        /**  All customizations that are NOT specific to a particular API-level can go here. 
+         */
+        public static final int AppNoTitleBarTheme=0x7f050002;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
