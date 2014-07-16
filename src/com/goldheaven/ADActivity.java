ea1d871e35e6345e3f86242305cdd97c9699fb6f
txidol <tanxiang163@163.com>
2014-07-16 18:00:45 +0800

    add

--- src/com/goldheaven/ADActivity.java
+++ src/com/goldheaven/ADActivity.java
@@ -5,6 +5,7 @@ import net.youmi.android.AdManager;
 import net.youmi.android.diy.DiyManager;
 import net.youmi.android.diy.banner.DiyAdSize;
 import net.youmi.android.diy.banner.DiyBanner;
+import net.youmi.android.offers.OffersManager;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,24 +26,36 @@ public class ADActivity extends Activity{
         setContentView(R.layout.ad);
         
         AdManager.getInstance(this).init("ee6a63e204914eda",
-				"6f7e2a256359c1db", false);
+				"6f7e2a256359c1db", true );
+        OffersManager.getInstance(this).onAppLaunch();
+        //展示全屏积分墙
+        OffersManager.getInstance(this).showOffersWall();
+
+    	//OffersManager.getInstance(this).showOffersWallDialog(this);
         
-        RelativeLayout adLayout=(RelativeLayout)findViewById(R.id.adLayout);
-        //demo 1 迷你Banner : 宽满屏，高32dp
-        DiyBanner banner = new DiyBanner(this, DiyAdSize.SIZE_MATCH_SCREENx32);//传入高度为32dp的AdSize来定义迷你Banner    
-        //demo 2 迷你Banner : 宽320dp，高32dp
-        //DiyBanner banner = new DiyBanner(this, DiyAdSize.SIZE_320x32);//传入高度为32dp的AdSize来定义迷你Banner 
-        //将积分Banner加入到布局中
-        adLayout.addView(banner);
+//        RelativeLayout adLayout=(RelativeLayout)findViewById(R.id.adLayout);
+//        //demo 1 迷你Banner : 宽满屏，高32dp
+//        DiyBanner banner = new DiyBanner(this, DiyAdSize.SIZE_MATCH_SCREENx32);//传入高度为32dp的AdSize来定义迷你Banner    
+//        //demo 2 迷你Banner : 宽320dp，高32dp
+//        //DiyBanner banner = new DiyBanner(this, DiyAdSize.SIZE_320x32);//传入高度为32dp的AdSize来定义迷你Banner 
+//        //将积分Banner加入到布局中
+//        adLayout.addView(banner);
         
         showRecommendAppWallBtn = (Button) findViewById(R.id.showRecommendAppWall);
         showRecommendAppWallBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
             	// 展示所有应用推荐墙
-            	DiyManager.showRecommendWall(ADActivity.this);
+            	//DiyManager.showRecommendWall(ADActivity.this);
+            	
             }
         });
         //DiyManager.showRecommendWall(ADActivity.this);
     }
+	
+	
+	@Override
+	protected void onDestroy() {
+		OffersManager.getInstance(this).onAppExit();
+	}
 }
