475329c2c8c50734cd5f8be7acc092c341131554
txidol <tanxiang163@163.com>
2014-07-16 16:57:35 +0800

    add

--- gen/com/goldheaven/R.java
+++ gen/com/goldheaven/R.java
@@ -11,17 +11,17 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int button_blue_bg=0x7f020000;
-        public static final int buttonbg=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int buttonbg=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f070005;
-        public static final int adLayout=0x7f070002;
-        public static final int diy=0x7f070001;
+        public static final int action_settings=0x7f070006;
+        public static final int adLayout=0x7f070001;
+        public static final int diy=0x7f070003;
         public static final int jiFenBtn=0x7f070004;
-        public static final int showSpot=0x7f070000;
-        public static final int zhuanQianBtn=0x7f070003;
+        public static final int showRecommendAppWall=0x7f070000;
+        public static final int showSpot=0x7f070002;
+        public static final int zhuanQianBtn=0x7f070005;
     }
     public static final class layout {
         public static final int ad=0x7f030000;
@@ -38,6 +38,7 @@ public final class R {
         public static final int hintText=0x7f040009;
         public static final int jiFen=0x7f040007;
         public static final int plan=0x7f040005;
+        public static final int showApp=0x7f04000d;
         public static final int showSmartBanner=0x7f040004;
         public static final int showSpot=0x7f040003;
         public static final int size20sp=0x7f04000c;
