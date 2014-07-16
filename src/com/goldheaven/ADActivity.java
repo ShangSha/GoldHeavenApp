5f53337a87ba56f6dbe1b93b751490f5439cf87b
txidol <tanxiang163@163.com>
2014-07-16 17:34:13 +0800

    add

--- src/com/goldheaven/ADActivity.java
+++ src/com/goldheaven/ADActivity.java
@@ -1,8 +1,13 @@
 package com.goldheaven;
 
+import net.youmi.android.AdManager;
+import net.youmi.android.diy.DiyManager;
+import net.youmi.android.diy.banner.DiyAdSize;
+import net.youmi.android.diy.banner.DiyBanner;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.RelativeLayout;
 
 public class ADActivity extends Activity{
 
@@ -13,5 +18,18 @@ public class ADActivity extends Activity{
 		Log.i(TAG, "启动AD");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ad);
+        
+        AdManager.getInstance(this).init("ee6a63e204914eda",
+				"6f7e2a256359c1db", false);
+        
+        RelativeLayout adLayout=(RelativeLayout)findViewById(R.id.adLayout);
+        //demo 1 迷你Banner : 宽满屏，高32dp
+        DiyBanner banner = new DiyBanner(this, DiyAdSize.SIZE_MATCH_SCREENx32);//传入高度为32dp的AdSize来定义迷你Banner    
+        //demo 2 迷你Banner : 宽320dp，高32dp
+        //DiyBanner banner = new DiyBanner(this, DiyAdSize.SIZE_320x32);//传入高度为32dp的AdSize来定义迷你Banner 
+        //将积分Banner加入到布局中
+        adLayout.addView(banner);
+        
+        DiyManager.showRecommendWall(ADActivity.this);
     }
 }
